8c906754828e2c8af5b0344fb38912c4f186df7a
andrewyang96 <ayang14@illinois.edu>
2014-10-27 14:03:07 -0500

    Node.js + Express.js overhaul
    Replaced Jekyll with Node.js and Express.js

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require("fs");
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// declare breakline generator
+app.locals.br = function (numTimes) {
+    var result = ""
+    for (var i=0; i<numTimes; i++) {
+        result += "<br/>";
+    }
+    return result;
+};
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// get team info from JSON file
+app.locals.teamdata = require("./data.json");
+// console.log(app.locals.teamdata);
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
