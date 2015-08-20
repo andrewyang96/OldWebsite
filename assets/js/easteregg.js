d996c4956cda53d7dca40d55ed4429e4a79eb77f
Andrew Yang <ayang14@illinois.edu>
2015-08-20 17:53:26 -0500

    Add Konami code easter egg
    All your base are belong to us

---
+++ assets/js/easteregg.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	$(window).konami({
+		cheat: function () {
+			// ALL YOUR BASE ARE BELONG TO US
+			$("#splash-head").html("MAIN SCREEN TURN ON");
+			$("#my-icon").attr("src", "./assets/img/zerowing.png");
+			$("#my-icon").attr("alt", "CATS");
+			$("#my-icon").attr("title", "That's NOT me!");
+			$("#splash-lead").html("All your base are belong to us.<br/>You have no chance to survive make your time.");
+			
+			// Then smooth scroll to splash page
+		    $("body").animate({
+	            scrollTop: 0
+	        }, 500);
+		}
+	});
+});
