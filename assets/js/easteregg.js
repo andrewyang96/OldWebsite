4bd287578164f89ca6e7f1a6b1861987ee297c44
Andrew Yang <ayang14@illinois.edu>
2015-09-04 22:03:56 -0500

    Add duck army easter egg

--- assets/js/easteregg.js
+++ assets/js/easteregg.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+	// Preload audio
+	var duckArmyAudio = new Audio("./assets/duckarmy.mp3");
+
+	// ALL YOUR BASE EASTER EGG
 	$(window).konami({
 		cheat: function () {
 			// ALL YOUR BASE ARE BELONG TO US
@@ -12,6 +16,27 @@ $(document).ready(function () {
 		    $("body").animate({
 	            scrollTop: 0
 	        }, 500);
+		}
+	});
+
+	// DUCK ARMY EASTER EGG
+	$(window).konami({
+		code: [68, 85, 67, 75, 65, 82, 77, 89],
+		cheat: function () {
+			// DUX
+			$("#splash-head").html("DUCK ARMY");
+			$("#my-icon").attr("src", "./assets/img/evilduck.jpg");
+			$("#my-icon").attr("alt", "Evil Duck");
+			$("#my-icon").attr("title", "That's NOT me!");
+			$("#splash-lead").html("HOOOOOOOOOOOOOOOOOOOONK!!");
+
+			// Then smooth scroll to splash page
+			$("body").animate({
+	            scrollTop: 0
+	        }, 500);
+
+			// Finally play audio
+			duckArmyAudio.play();
 		}
 	});
 });
